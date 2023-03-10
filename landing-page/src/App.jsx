c856ec105440730e33acd76e93398070cc4a89dd
RezkiBawono <rezkibawono@gmail.com>
2023-03-10 10:47:18 +0700

    First commit

---
+++ landing-page/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello I am a Landing Page using Tailwind CSS
+      </h1>
+    </div>
+  );
+}
+
+export default App;
